2c3d4188492f4f460c896a6b7ddca48c4bb2db28
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-04-21 20:25:14 -0400

    Implemented all the required SQL queries in queries.sql file

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 
 const server = express();
+const accountsRouter = require("./accounts/accounts-router.js");
 
 server.use(express.json());
+
+server.use("/api/accounts", accountsRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: `Welcome to the Relational Database Management Sprint`
+    })
+})
 
 module.exports = server;
